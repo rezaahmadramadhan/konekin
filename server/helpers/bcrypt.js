b095834b5dc013203b152ca56da6a52671d8c191
rezaahmadramadhan <dhronzae@gmail.com>
2025-05-13 17:50:38 +0700

    feat: setting up bcryptjs

--- server/helpers/bcrypt.js
+++ server/helpers/bcrypt.js
@@ -1,3 +1,12 @@
-// const bcrypt = require('bcryptjs')
+const bcrypt = require("bcryptjs");
 
-// const hashPassword
+const hashPassword = (password) => {
+  const salt = bcrypt.genSaltSync(10);
+  return bcrypt.hashSync(password, salt);
+};
+
+const comparePassword = (password, hashedPassword) => {
+  return bcrypt.compareSync(password, hashedPassword);
+};
+
+module.exports = { hashPassword, comparePassword };
